5360b3e64a246672e45f2a6952ad75f2aa07ca09
DESKTOP-POSS0RQ\olega <olegackiv956@gmail.com>
2022-06-12 15:44:14 +0300

    Initial commit

---
+++ src/test/java/ua/lviv/iot/novaPoshtaAPI/NovaPoshtaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.novaPoshtaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaPoshtaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
